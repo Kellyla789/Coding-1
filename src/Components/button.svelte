2600db59af48013187b8318110ff651a3de4cb47
Kellyla789 <kelly.la789@gmail.com>
2023-08-16 18:44:32 -0400

    publish gh pages

--- src/Components/button.svelte
+++ src/Components/button.svelte
@@ -1,15 +1,3 @@
 <button on:click>
-    <slot></slot>
+    <slot />
 </button>
-
-<style>
-    button {
-        color: white;
-        background-color: #61baa8;
-        box-shadow: 0cm;
-        opacity: 0.75;
-        border-radius: 30%;
-        font-size: 38px;
-        font-weight: bold;
-    }
-</style>
---
+++ src/Components/input.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let value: string | number;
+</script>
+
+<label><slot /></label>
+<input bind:value type="text" />
+
+<style>
+    input {
+        border: 1px solid;
+        background: white;
+        font-size: 16px;
+    }
+</style>
